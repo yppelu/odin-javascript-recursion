d08fd5800991887b124dd2e7122e1f681ec8b096
yppelu <yppelu@gmail.com>
2023-11-26 15:59:01 +0700

    Complete fibs function

---
+++ assignment1/fibs.js
@@ -0,0 +1,15 @@
+function fibs(n) {
+  if (n < 0) return 'Invalid input';
+  if (n < 1) return [0];
+
+  const fibs = [0, 1];
+  for (let i = 2; i <= n; i++)  fibs[i] = fibs[i - 1] + fibs[i - 2];
+  return fibs;
+}
+
+console.log(fibs(-1));
+console.log(fibs(0));
+console.log(fibs(1));
+console.log(fibs(2));
+console.log(fibs(5));
+console.log(fibs(19));
